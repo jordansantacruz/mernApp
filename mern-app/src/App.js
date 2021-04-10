b9165b5bc819db8d39fabe1ef10ab30715c7b2bd
Jordan Santacruz <jordansantacruz713@gmail.com>
2021-04-10 19:44:51 -0400

    Added Homepage

--- mern-app/src/App.js
+++ mern-app/src/App.js
@@ -4,11 +4,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import Login from "./components/login.component";
 import CreateUser from "./components/createUser.component";
+import Homepage from "./components/homepage.component";
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={Login}/>
+      <Route path="/homepage" exact component={Homepage}/>
       <Route path="/login" exact component={Login}/>
       <Route path="/create-user" exact component={CreateUser}/>
     </Router>
